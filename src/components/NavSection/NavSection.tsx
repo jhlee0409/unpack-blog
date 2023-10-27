717146762f86a29f304a770b96ad33285ff4a4d9
jhlee0409 <relee6203@gmail.com>
2023-10-28 01:33:40 +0900

    feat: add base nav

--- src/components/NavSection/NavSection.tsx
+++ src/components/NavSection/NavSection.tsx
@@ -1,13 +1,40 @@
 "use client";
+import Link from "next/link";
 import classes from "./NavSection.module.scss";
+import { useState } from "react";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(classes);
 
 export const NavSection = () => {
+  const [active, setActive] = useState(1);
   return (
     <div className={classes.container}>
       <div className={classes.titleBox}>
         <span>{`<Unpack>`}</span>
       </div>
-      <nav>1</nav>
+      <nav className={classes.linkGroup}>
+        <Link
+          className={cx("link", active === 1 ? "active" : "")}
+          href="/"
+          onClick={() => setActive(1)}
+        >
+          <div
+            className={classes.bgBox}
+            style={{ transform: `translateY(calc(-50% + ${active - 1}00%))` }}
+          >
+            <div />
+          </div>
+          section
+        </Link>
+        <Link
+          className={cx("link", active === 2 ? "active" : "")}
+          href="/"
+          onClick={() => setActive(2)}
+        >
+          section1
+        </Link>
+      </nav>
     </div>
   );
 };
